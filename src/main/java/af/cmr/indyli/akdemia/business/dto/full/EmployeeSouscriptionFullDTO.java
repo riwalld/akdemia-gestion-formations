7b4df9bd1a1da06a4a45bc1333c5439f6669a75b
user <test>
2024-03-11 09:50:53 +0100

    completion of the mediumDTO according to the entities

---
+++ src/main/java/af/cmr/indyli/akdemia/business/dto/full/EmployeeSouscriptionFullDTO.java
@@ -0,0 +1,12 @@
+package af.cmr.indyli.akdemia.business.dto.full;
+
+import af.cmr.indyli.akdemia.business.dto.medium.EmployeeSouscriptionMediumDTO;
+
+/**
+ * This class represents a basic Data Transfer Object (DTO) for a {@link af.cmr.indyli.akdemia.business.entity.EmployeeSouscription}
+ * entity.
+ */
+
+public class EmployeeSouscriptionFullDTO extends EmployeeSouscriptionMediumDTO {
+
+}

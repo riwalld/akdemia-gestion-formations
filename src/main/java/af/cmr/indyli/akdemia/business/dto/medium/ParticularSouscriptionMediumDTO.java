f399094e56d5587e170a9c3ee88e86916671963d
user <test>
2024-03-11 09:53:21 +0100

    completion of the mediumDTO according to the entities

---
+++ src/main/java/af/cmr/indyli/akdemia/business/dto/medium/ParticularSouscriptionMediumDTO.java
@@ -0,0 +1,14 @@
+package af.cmr.indyli.akdemia.business.dto.medium;
+
+import af.cmr.indyli.akdemia.business.dto.basic.ParticularSouscriptionBasicDTO;
+import af.cmr.indyli.akdemia.business.entity.ParticularSouscription;
+
+/**
+ * This class represents a medium Data Transfer Object (DTO) for a
+ * {@link ParticularSouscription} entity. It extends {@link ParticularSouscription} and inherits basic
+ * information about a manager. Medium DTOs typically include additional details
+ * beyond the basic DTO but exclude complex associations like lists.
+ */
+
+public class ParticularSouscriptionMediumDTO extends ParticularSouscriptionBasicDTO {
+}
